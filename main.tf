068c55099936e2a7d4dadab04c6b4a16e4a6aa39
Romain Decker <romain@cloudmaniac.net>
2020-03-29 14:02:07 +0200

    Small modification in the provider included in the module

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@ provider "aws" {
   alias                   = "us-east-1"
   version                 = "~> 2.0"
   region                  = "us-east-1"
-  shared_credentials_file = "../.aws/credentials"
-  profile                 = "romain"
 }
 
 ## Route 53
